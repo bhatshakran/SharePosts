c88597ad61fd57612500202a7e70291d8eb4a0e3
shaqran_bhat <bhatshakran@gmail.com>
2021-06-09 20:41:42 +0530

    posts fetched from the db and rendered in the ui

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -4,11 +4,19 @@
             if(!isLoggedIn()){
                 redirect('users/login');
             }
+
+            $this->postModel = $this->model('Post');
         }
 
 
         public function index() {
-            $data =[];
+            // Get posts
+
+            $posts = $this->postModel->getPosts();
+
+            $data =[
+                'posts' => $posts
+            ];
 
             $this->view('posts/index', $data);
         }
--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -10,4 +10,19 @@
             Add Post</a>
     </div>
 </div>
+<?php foreach($data['posts'] as $post) : ?>
+    <div class="card card-body mb-3">
+        <h4 class="card-title">
+            <?php echo $post->title; ?>
+        </h4>
+        <div class="bg-light p-2 mb-3">
+            Written by <?php echo $post->name; ?>
+            on <?php echo $post->postCreated; ?>
+        </div>
+        <p class="card-text">
+            <?php echo $post->body; ?>
+        </p>
+        <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->postId; ?>" class="btn btn-dark">More</a>
+    </div>
+<?php endforeach; ?>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
